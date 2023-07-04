1972efad3288d62ebaa9180aa63cd760cff40433
JinyongJeong <jjy0923@gmail.com>
2023-07-04 14:13:16 +0000

    Update strategy pattern

--- example/design_pattern/strategy_patter.cpp
+++ example/design_pattern/strategy_patter.cpp
@@ -1,7 +1,10 @@
+#include <iostream>
 #include <vector>
 
 class Item {
  public:
+  Item(float price, bool is_fresh) : price_(price), is_fresh_(is_fresh){};
+
   bool is_fresh() { return is_fresh_; }
   float price() { return price_; }
 
@@ -12,7 +15,8 @@ class Item {
 
 class DiscountStrategy {
  public:
-  virtual float GetDiscountPrice(Item item) {}
+  virtual float GetDiscountPrice(
+      Item item) = 0;  // 각 Strategy 로 overwrite 할 함수
 };
 
 class FirstCustomerDiscountStrategy : public DiscountStrategy {
@@ -36,34 +40,49 @@ class LastCustomerDiscountStrategy : public DiscountStrategy {
   }
 };
 
-DiscountStrategy strategy;
+DiscountStrategy* strategy;  // 다형성(Polymorphism)은 pointer 일때 가능하다!
 
 class Calculator {
  public:
-  Calculator(DiscountStrategy strategy) : strategy_(strategy) {}
+  Calculator(DiscountStrategy* strategy) : strategy_(strategy) {}
   float Calculate(
       std::vector<Item> items) {  // 계산하는 부분은 전략에 따라 변하지 않음
     float sum = 0.0;
     for (auto& item : items) {
-      sum += strategy_.GetDiscountPrice(item);
+      sum += strategy_->GetDiscountPrice(item);
     }
     return sum;
   }
 
  private:
-  DiscountStrategy strategy_;
+  DiscountStrategy* strategy_;
 };
 
 void PushFirstCustomerButtom() {  // 버튼 누를 때 전략 선택
-  strategy = FirstCustomerDiscountStrategy();
+  std::cout << "First Customer Button is pushed" << std::endl;
+  strategy = new FirstCustomerDiscountStrategy();
 };
 void PushLastCustomerButton() {  // 버튼 누를 때 전략 선택
-  strategy = LastCustomerDiscountStrategy();
+  std::cout << "Last Customer Button is pushed" << std::endl;
+  strategy = new LastCustomerDiscountStrategy();
 };
 void PushCalculationButton() {  // 실제 계산 실행 버튼
   Calculator calculator(strategy);
   std::vector<Item> items;
+  Item item1(100.0, false);
+  Item item2(200.0, true);
+  items.push_back(item1);
+  items.push_back(item2);
   float result = calculator.Calculate(items);
+  std::cout << "Calculated price is " << result << std::endl;
 };
 
-int main() { return 0; }
+int main() {
+  PushFirstCustomerButtom();
+  PushCalculationButton();
+
+  PushLastCustomerButton();
+  PushCalculationButton();
+
+  return 0;
+}
